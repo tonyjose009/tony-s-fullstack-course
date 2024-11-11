43f0f4d145eb779f362fde80b3548d462ea5332e
tonyjose009 <tonyjose009@gmail.com>
2024-11-11 12:43:18 +0000

    Finished 1.1

---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,9 @@
+const course = 'Half Stack application development'
+
+const Header = () => {
+    return (
+        <h1>{course}</h1>
+    )
+}
+
+export default Header
---
+++ part1/courseinfo/src/Total.jsx
@@ -0,0 +1,9 @@
+const Total = () => {
+    const exercises1 = 10, exercises2 = 7, exercises3 = 14
+
+    return (
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    )
+}
+
+export default Total
