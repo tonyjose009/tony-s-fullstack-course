042161f09c821b4fe0d2069db966c6f1ec533db3
tonyjose009 <tonyjose009@gmail.com>
2024-11-18 12:23:15 +0000

    Finished till part 1.c

--- part1/rerender/src/App.jsx
+++ part1/rerender/src/App.jsx
@@ -1,18 +1,33 @@
 import {useState} from "react";
 
-const App = ()  => {
+const Display = ({counter}) => { return (<div className="app">Counter={counter}</div>) }
+
+const Button = ({onClick, label}) => { return (<button onClick={onClick}>{label}</button>)}
+
+
+
+const App = () => {
     const [counter, setCounter] = useState(0)
 
-    const increaseCounter = () => setCounter(counter + 1)
-    const decreaseCounter = () => setCounter(counter - 1)
-    const resetCounter = () => setCounter(0);
+    const increaseCounter = () => {
+        console.log('Going to increase counter from ', counter);
+        setCounter(counter + 1)
+    }
+    const decreaseCounter = () => {
+        console.log('Going to decrease counter from ', counter);
+        setCounter(counter - 1)
+    }
+    const resetCounter = () => {
+        console.log('Going to reset counter from ', counter)
+        setCounter(0)
+    }
     console.log('rendering...', counter)
     return (
         <div>
-        <div className="App">Counter {counter}</div>
-            <button onClick={increaseCounter}>Increase </button>
-            <button onClick={decreaseCounter}>Decrease </button>
-            <button onClick={resetCounter}>Reset</button>
+            <Display counter={counter} />
+            <Button onClick={increaseCounter} label="Increase" />
+            <Button onClick={decreaseCounter} label="Decrease" />
+            <Button onClick={resetCounter} label="Reset" />
         </div>
     )
 }
