175e48703a6f9faba7a5b1b553aed770ab69823a
tonyjose009 <tonyjose009@gmail.com>
2024-11-25 15:00:16 +0000

    Added new component to separate each statistic

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,26 +6,46 @@ const Button = ({onClickHandler, text}) => {
     )
 }
 
-const App = ()  =>{
+const StatisticLine = (props) => {
+    return (
+        <p>{props.label} = {props.value}</p>
+    )
+}
+
+const Statistics = (props) => {
+    let good = props.good, neutral = props.neutral, bad = props.bad
+    let  total = good + neutral+ bad, avg = 0 , positive =  0
+    if(total === 0){
+        return  (
+            <div>
+                <h2>Statistics</h2>
+                <p>No Feedback given</p>
+            </div>
+        )
+    } else {
+        if(total !== 0) {
+            avg = ((good * 1) + (bad * -1)) / (good + neutral + bad)
+            positive = (good / total) * 100
+        }
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <StatisticLine label="Good" value={good} />
+                <StatisticLine label="Neutral" value={neutral} />
+                <StatisticLine label="Bad" value={bad} />
+                <StatisticLine label="All" value={total} />
+                <StatisticLine label="Average" value={avg} />
+                <StatisticLine label="Positive" value={positive} />
+            </div>
+        )
+    }
+}
+
+
+const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
-    const Statistics = (props) => {
-        let  total = good + neutral + bad, avg = 0 , positive =  0
-        if(total !== 0) {
-             avg = ((good * 1) + (bad * -1)) / (good + neutral + bad)
-             positive = (good / total) * 100
-        }
-
-        if(props.type === 'total'){
-            return total
-        } else if (props.type === 'avg'){
-            return  avg
-        } else if (props.type === 'pos'){
-            return positive
-        }
-    }
 
     return (
         <div>
@@ -33,13 +53,7 @@ const App = ()  =>{
             <Button onClickHandler={ () => {setGood(good+1)}} text="Good" />
             <Button onClickHandler={ () => {setNeutral(neutral+1)}} text="Neutral" />
             <Button onClickHandler={ () => {setBad(bad+1)}} text="Bad" />
-            <h2>Statistics</h2>
-            <p>Good = {good}</p>
-            <p>Neutral = {neutral}</p>
-            <p>Bad = {bad}</p>
-            <p>All = {Statistics({type:'total'})}</p>
-            <p>Average = {Statistics({type:'avg'})}</p>
-            <p>Positive = {Statistics({type:'pos'})}</p>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
